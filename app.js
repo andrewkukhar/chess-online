0f1feb04605890adf0d6f023f6bbb4b78de8184a
andreyrise <andrey.rise@gmail.com>
2022-11-20 12:21:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const http = require('http')
+const cors = require("cors");
+const path = require('path')
+const socketio = require('socket.io')
+const gameLogic = require('./game-logic.js')
+const app = express()
+
+
+/**
+ * Backend flow:
+ * - check to see if the game ID encoded in the URL belongs to a valid game session in progress. 
+ * - if yes, join the client to that game. 
+ * - else, create a new game instance. 
+ * - '/' path should lead to a new game instance. 
+ * - '/game/:gameid' path should first search for a game instance, then join it. Otherwise, throw 404 error.  
+ */
+
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+// get the gameID encoded in the URL. 
+// check to see if that gameID matches with all the games currently in session. 
+// join the existing game session. 
+// create a new session.  
+// run when client connects
+
+io.on('connection', client => {
+    gameLogic.initializeGame(io, client)
+})
+
+// usually this is where we try to connect to our DB.
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
