4f57f38b367d4cc678b61a7b47560c1344b3dc6b
andreyrise <andrey.rise@gmail.com>
2022-11-20 13:59:22 -0800

    some changes

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const express = require('express')
-const http = require('http')
+require("dotenv").config();
 const cors = require("cors");
 const path = require('path')
-const socketio = require('socket.io')
-const gameLogic = require('./game-logic.js')
 const app = express()
 
-
-/**
- * Backend flow:
- * - check to see if the game ID encoded in the URL belongs to a valid game session in progress. 
- * - if yes, join the client to that game. 
- * - else, create a new game instance. 
- * - '/' path should lead to a new game instance. 
- * - '/game/:gameid' path should first search for a game instance, then join it. Otherwise, throw 404 error.  
- */
-
-
-const server = http.createServer(app)
-const io = socketio(server)
-
-// get the gameID encoded in the URL. 
-// check to see if that gameID matches with all the games currently in session. 
-// join the existing game session. 
-// create a new session.  
-// run when client connects
-
-io.on('connection', client => {
-    gameLogic.initializeGame(io, client)
-})
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // usually this is where we try to connect to our DB.
 app.use('/', express.static(path.join(__dirname, 'client', 'build')))
@@ -37,7 +18,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
