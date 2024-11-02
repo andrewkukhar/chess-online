7c0c2641ac89a0fc93792cc1ed4d7f362e9235c5
andrewkukhar <andriikukharv@gmail.com>
2024-11-01 17:03:02 -0700

    adjusted notifs and seo and something else

--- client/src/contexts/NotificationContext.js
+++ client/src/contexts/NotificationContext.js
@@ -1,12 +1,13 @@
 // src/contexts/NotificationContext.js
 import React, { createContext, useState, useRef, useEffect } from "react";
-import { Snackbar, Alert } from "@mui/material";
+import { Snackbar, Alert, useMediaQuery } from "@mui/material";
 
 export const NotificationContext = createContext();
 
 export const NotificationProvider = ({ children }) => {
   const [notifications, setNotifications] = useState([]);
   const timeoutRefs = useRef([]);
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   const addNotification = (message, severity = "info") => {
     const id = new Date().getTime();
@@ -15,7 +16,9 @@ export const NotificationProvider = ({ children }) => {
       : 3000;
 
     setNotifications((prevNotifications) => {
-      if (prevNotifications.length >= 5) {
+      if (prevNotifications.length >= 3) {
+        prevNotifications.shift();
+      } else if (isSmallScreen && prevNotifications.length >= 2) {
         prevNotifications.shift();
       }
       return [
@@ -50,16 +53,33 @@ export const NotificationProvider = ({ children }) => {
   return (
     <NotificationContext.Provider value={{ addNotification }}>
       {children}
-      {notifications.map((notification, index) => (
+      {notifications?.map((notification, index) => (
         <Snackbar
-          key={notification.id}
+          key={notification?.id}
           open={true}
-          autoHideDuration={notification.autoHideDuration}
-          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-          style={{ bottom: `${index * 60}px` }}
+          autoHideDuration={notification?.autoHideDuration}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          className={`notification-snackbar notification-snackbar-${
+            index === 0
+              ? "0"
+              : index === 1
+              ? "1"
+              : index === 2
+              ? "2"
+              : index === 3
+              ? "3"
+              : index === 4
+              ? "4"
+              : index === 5
+              ? "5"
+              : ""
+          }`}
         >
-          <Alert severity={notification.severity} sx={{ width: "100%" }}>
-            {notification.message}
+          <Alert
+            severity={notification?.severity}
+            className="notification-alert"
+          >
+            {notification?.message}
           </Alert>
         </Snackbar>
       ))}
